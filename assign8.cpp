5944ad01da5ac73e74c2f0abf208398a2053fc22
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-11-19 14:41:10 -0600

    Initial commit.

---
+++ assign8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main(int argc, char* argv[])
+{
+    string command;
+
+    cout << "Enter command: " << endl;
+    cin >> command;
+
+    cout << command << endl;
+
+    return 0;
+}
