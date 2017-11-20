5800386dcab467361d60fa69e3fc19048609e5d7
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-11-20 14:15:47 -0600

    Progress.

--- assign8.cpp
+++ assign8.cpp
@@ -4,48 +4,72 @@
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
+#include <cstring>
 using namespace std;
  
 int main() {
-	
-   while (true)
-   {
-       string command;
-       string argumentOne;
-       string argumentTwo;
-       string argumentThree; 
 
-       // prompt user for command to run
-       cout << "Enter command to run: ";
-       cin >> command >> argumentOne >> argumentTwo >> argumentThree;
-       
-       int pid, rs, status;
+    char buffer[80];
 
-       // fork will make 2 processes
-       pid = fork();
-       if (pid == -1)
-       { 
+    while (true)
+    {
+        string command;
+        char argument[4];
+        char options[4];
+
+        // prompt user for command to run
+        cout << "Enter a command to run: ";
+
+        cin.getline(buffer, 80);
+        
+        if (!strcmp(buffer, "exit"))
+        {
+            break;
+        }
+
+        cout << buffer << endl;
+
+        char * pch;
+        pch = strtok (buffer, " ");
+        while (pch != NULL)
+        {
+            pch = strtok (NULL, " ");
+        }
+
+        //
+        //
+        //
+        //  This is where the magic starts
+        //
+        //
+        //
+        /*
+        int pid, rs, status;
+
+        // fork will make 2 processes
+        pid = fork();
+        if (pid == -1)
+        { 
            perror("fork");
            exit(EXIT_FAILURE);
-       }
-     
-       if (pid == 0)
-       {
-          // Child process: exec to command with argument
-          // cout << "before exec to: " << command << " " << argumentOne << " " << argumentTwo << " " << argumentThree << " " << endl;
-          rs = execlp(command.c_str(), command.c_str(), argumentOne.c_str(), argumentTwo.c_str(), argumentThree.c_str(), (char*) NULL);
+        }
+
+        if (pid == 0)
+        {
+          rs = execlp("touch", "touch", buffer, (char*) NULL);
           
           if (rs == -1)
           {
               perror("execlp"); exit(EXIT_FAILURE); 
           }
-       }
-       else
-       { 
+        }
+        else
+        { 
           // Parent process: wait for child to end
           wait(&status);
-       }
+        }
 
-   }
-   return 0;
+        */
+    }
+        return 0;
 }
